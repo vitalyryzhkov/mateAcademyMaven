eeeaaab2a7221d520b8fff645091bba86d921a36
Vitaliy <pentagon0555@gmail.com>
2019-03-24 18:29:33 +0200

    done

--- src/test/java/com/homework/lesson09/MyStackTest.java
+++ src/test/java/com/homework/lesson09/MyStackTest.java
@@ -42,14 +42,29 @@ public class MyStackTest {
 
     @Test
     public void peek() {
-
+        myStack.push(1);
+        myStack.push(2);
+        int expected = myStack.peek();
+        int actual = 2;
+        assertEquals(expected, actual);
     }
 
     @Test
     public void getSize() {
+        myStack.push(1);
+        myStack.push(1);
+        int expected = myStack.getSize();
+        int actual = 2;
+        assertEquals(expected, actual);
     }
 
     @Test
     public void clear() {
+        myStack.push(1);
+        myStack.push(1);
+        myStack.clear();
+        int expected = myStack.getSize();
+        int actual = 0;
+        assertEquals(expected, actual);
     }
 }
