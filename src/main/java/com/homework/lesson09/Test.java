c4d79d4126c5146398a016ebf1b1b95c6ff470dc
Vitaliy <pentagon0555@gmail.com>
2019-03-24 18:25:20 +0200

    done

--- src/main/java/com/homework/lesson09/Test.java
+++ src/main/java/com/homework/lesson09/Test.java
@@ -2,7 +2,6 @@ package com.homework.lesson09;
 
 public class Test {
     public static void main(String[] args) {
-        String newHelloWorld = "Hello World";
-        System.out.println(newHelloWorld + "!");
+
     }
 }
