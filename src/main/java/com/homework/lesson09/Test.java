3c5b36698077f7d66155a3538a9dbd835bfc7999
Vitaliy <pentagon0555@gmail.com>
2019-03-24 15:17:39 +0200

    test

---
+++ src/main/java/com/homework/lesson09/Test.java
@@ -0,0 +1,8 @@
+package com.homework.lesson09;
+
+public class Test {
+    public static void main(String[] args) {
+        String newHelloWorld = "Hello World";
+        System.out.println(newHelloWorld + "!");
+    }
+}
