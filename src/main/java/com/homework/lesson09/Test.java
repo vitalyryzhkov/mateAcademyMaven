80a558792081aabd4177be50d191519b4d4375e8
vitalyryzhkov <pentagon0555@gmail.com>
2019-03-24 15:21:55 +0200

    Delete Test.java

--- src/main/java/com/homework/lesson09/Test.java
+++
@@ -1,8 +0,0 @@
-package com.homework.lesson09;
-
-public class Test {
-    public static void main(String[] args) {
-        String newHelloWorld = "Hello World";
-        System.out.println(newHelloWorld + "!");
-    }
-}
