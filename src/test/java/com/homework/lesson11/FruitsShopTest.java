a8d034cadec2175093407cad60ee18152ac210f0
Vitaliy <pentagon0555@gmail.com>
2019-04-03 19:27:18 +0300

    fixed

--- src/test/java/com/homework/lesson11/FruitsShopTest.java
+++ src/test/java/com/homework/lesson11/FruitsShopTest.java
@@ -1,60 +1,58 @@
 package com.homework.lesson11;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.homework.lesson11pr.Fruits;
-import com.homework.lesson11pr.FruitsShop;
-import com.homework.lesson11pr.FruitsType;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class FruitsShopTest {
 
     ObjectMapper objectMapper;
     private FruitsShop fruitsShop;
-    private List<Fruits> fruitsList;
+    private List<Fruits> fruitsList = new ArrayList<>();
 
     @Before
     public void setUp() {
+        Fruits pear = new Fruits();
         fruitsShop = new FruitsShop();
+        pear.setType(FruitsType.PEAR);
+        pear.setDateDelivery(2019, 3, 7);
+        pear.setPrice(30);
+        fruitsList.add(pear);
     }
 
-    @Test
-    public void addFruits() {
-        try {
-            fruitsShop.addFruits("src/main/resources/fruitApple.json");
-        } catch (IOException e) {
-            e.getMessage();
-        }
-    }
-
-    @Test
-    public void save() {
-        try {
-            fruitsShop.save("src/main/resources/fruitApple.json");
-        } catch (IOException e) {
-            e.getMessage();
-        }
-    }
-
-    @Test
-    public void load() {
-        try {
-            fruitsShop.load("src/main/resources/fruitApple.json");
-        } catch (IOException e) {
-            e.getMessage();
-        }
-    }
+//    @Test
+//    public void addFruits() {
+//        fruitsShop.addFruits("src/main/resources/fruitApple.json");
+//        assertEquals(4, fruitsShop.getFruitsArrayList().size());
+//    }
+//
+//    @Test
+//    public void save() {
+//        fruitsShop.save("src/main/resources/fruitApple.json");
+//        assertEquals(4, fruitsShop.getFruitsArrayList().size());
+//    }
+//
+//    @Test
+//    public void load() {
+//        fruitsShop.load("src/main/resources/fruitApple.json");
+//        assertEquals(4, fruitsShop.getFruitsArrayList().size());
+//    }
 
     @Test
     public void getSpoiledFruits() {
         int countSpoiledFruits = fruitsShop.getSpoiledFruits(LocalDate.of(2017, 1, 1)).size();
+        int countSpoiledFruits2 = fruitsShop.getSpoiledFruits(LocalDate.of(2019, 5, 5)).size();
         assertEquals((countSpoiledFruits), 0);
+        assertEquals((countSpoiledFruits2), 1);
     }
 
     @Test
