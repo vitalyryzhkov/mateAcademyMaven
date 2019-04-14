c2bc76b7ee5e058da8fb84313b10ff20375c8e58
Vitaliy <pentagon0555@gmail.com>
2019-04-15 02:51:22 +0300

    fixed final fields names

--- src/main/java/com/homework/lesson14/abstractfactory/AbstractFactory.java
+++ src/main/java/com/homework/lesson14/abstractfactory/AbstractFactory.java
@@ -1,15 +1,15 @@
 package com.homework.lesson14.abstractfactory;
 
 public class AbstractFactory {
-    private static final String uaFactory = "UA";
-    private static final String ruFactory = "RU";
+    private static final String UA_FACTORY = "UA";
+    private static final String RU_FACTORY = "RU";
 
     public static InteAbsFactory getFactory(String country) {
 
         InteAbsFactory ifactory = null;
-        if (country.equals(uaFactory)) {
+        if (country.equals(UA_FACTORY)) {
             ifactory = new UaCarPriceAbsFactory();
-        } else if (country.equals(ruFactory)) {
+        } else if (country.equals(RU_FACTORY)) {
             ifactory = new RuCarPriceAbsFactory();
         }
         return ifactory;
